fc4d1cf8433e0a51ea541601113a6e1a53e5670d
Dhyan Tanna <79247409+D99-1@users.noreply.github.com>
2024-07-13 00:14:41 +1000

    complete refresh

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
-const playBtn = document.querySelector('.play-btn');
-const pauseBtn = document.querySelector('.pause-btn');
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-const coverArt = document.querySelector('.cover-art');
-const musicPlayer = document.querySelector('.music-player');
-const songTitle = document.querySelector('.title');
-const artistName = document.querySelector('.artist');
+function requestFullScreen(element) {
+    // Supports most browsers and their versions.
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
 
+    if (requestMethod) { // Native full screen.
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
 
-
-
-// fetch video data from API and update UI with song details and cover art
-fetch('http://localhost:13090/track')
-	.then(response => response.json())
-	.then(data => {
-		songTitle.textContent = data.video.title;
-		artistName.textContent = data.author;
-		coverArt.style.backgroundImage = `url(${data.thumbnail.thumbnails[0].url})`;
-	})
-	.catch(error => console.log(error));
-
-
-// get video information from API and update UI
-fetch('http://localhost:13090/track')
-	.then(response => response.json())
-	.then(data => {
-		songTitle.textContent = data.video.title;
-		artistName.textContent = data.author;
-		coverArt.style.backgroundImage = `url(${data.thumbnail.thumbnails[0].url})`;
-	});
-
+var elem = document.body; 
+document.onload = requestFullScreen(elem);
