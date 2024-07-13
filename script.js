69a5f450ea34cd2943e426239d567cc71bee7222
Dhyan Tanna <79247409+D99-1@users.noreply.github.com>
2024-07-13 14:45:55 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-function requestFullScreen(element) {
-    // Supports most browsers and their versions.
-    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
-
-    if (requestMethod) { // Native full screen.
-        requestMethod.call(element);
-    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
-        var wscript = new ActiveXObject("WScript.Shell");
-        if (wscript !== null) {
-            wscript.SendKeys("{F11}");
-        }
-    }
+function secondsToTime(seconds){
+    let minutes = Math.floor(seconds / 60);
+    let remainingSeconds = seconds % 60;
+    return minutes + ':' + (remainingSeconds < 10 ? '0' : '') + remainingSeconds;
 }
 
-var elem = document.body; 
-document.onload = requestFullScreen(elem);
+function getData(){
+    console.log('Getting data from server');
+    fetch('http://192.168.0.162:13091')
+    .then(response => response.json())
+    .then(data => {
+        console.log("playing: " + data.playing);
+        document.getElementById('play-pause-btn').src = data.playing ? './assets/Pause square.png' : './assets/Play alt.png';
+        document.getElementById('thumbnail').src = data.image;
+        document.getElementById('title').innerText = data.title;
+        document.getElementById('artist').innerText = data.artist;
+        document.getElementById('progress').innerText = secondsToTime(data.progress)
+        document.getElementById('length').innerText = secondsToTime(data.length)
+        document.getElementById('progress-bar').style.width = (data.progress / data.length) * 100 + '%';
+        document.getElementById('progress-bar').style.backgroundColor = `rgb(${data.accentr}, ${data.accentg}, ${data.accentb})`;
+        document.getElementById('thumbnail-background').style.backgroundImage = `url('${data.image}')`;
+    });
+}
+getData()
+setInterval(getData, 30000);
+
+document.getElementById('play-pause-btn').addEventListener('click', () => {
+    fetch('http://192.168.0.162:13091/volume/togglePausePlay')
+    .then(() => getDataAfterTimeout());
+}
+);
+
+function getDataAfterTimeout(){
+    setTimeout(getData, 100);
+}
+
+
