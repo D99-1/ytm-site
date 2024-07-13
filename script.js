dcbb21e62781f4d81415f9f6907582197244bbe2
Dhyan Tanna <79247409+D99-1@users.noreply.github.com>
2024-07-13 16:08:07 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let progressInterval;
+
+function autoIncrementTime(){
+    let time = document.getElementById('progress').innerText;
+    let timeArray = time.split(':');
+    let minutes = parseInt(timeArray[0]);
+    let seconds = parseInt(timeArray[1]);
+    seconds++;
+    if(seconds >= 60){
+        seconds = 0;
+        minutes++;
+    }
+    document.getElementById('progress').innerText = minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+    document.getElementById('progress-bar').style.width = (minutes * 60 + seconds) / (parseInt(document.getElementById('length').innerText.split(':')[0]) * 60 + parseInt(document.getElementById('length').innerText.split(':')[1])) * 100 + '%';
+}
+
 function secondsToTime(seconds){
     let minutes = Math.floor(seconds / 60);
     let remainingSeconds = seconds % 60;
@@ -9,6 +25,7 @@ function getData(){
     fetch('http://192.168.0.162:13091')
     .then(response => response.json())
     .then(data => {
+        clearInterval(progressInterval);
         console.log("playing: " + data.playing);
         document.getElementById('play-pause-btn').src = data.playing ? './assets/Pause square.png' : './assets/Play alt.png';
         document.getElementById('thumbnail').src = data.image;
@@ -19,6 +36,9 @@ function getData(){
         document.getElementById('progress-bar').style.width = (data.progress / data.length) * 100 + '%';
         document.getElementById('progress-bar').style.backgroundColor = `rgb(${data.accentr}, ${data.accentg}, ${data.accentb})`;
         document.getElementById('thumbnail-background').style.backgroundImage = `url('${data.image}')`;
+        if(data.playing){
+        progressInterval = setInterval(autoIncrementTime, 1000);
+        }
     });
 }
 getData()
@@ -26,12 +46,22 @@ setInterval(getData, 30000);
 
 document.getElementById('play-pause-btn').addEventListener('click', () => {
     fetch('http://192.168.0.162:13091/volume/togglePausePlay')
-    .then(() => getDataAfterTimeout());
+    .then(() => getDataAfterTimeout(100));
 }
 );
 
-function getDataAfterTimeout(){
-    setTimeout(getData, 100);
+document.getElementById('prev-btn').addEventListener('click', () => {
+    fetch('http://192.168.0.162:13091/prev')
+    .then(() => getDataAfterTimeout(500));
 }
+);
 
+document.getElementById('next-btn').addEventListener('click', () => {
+    fetch('http://192.168.0.162:13091/next')
+    .then(() => getDataAfterTimeout(500));
+}
+);
 
+function getDataAfterTimeout(ms){
+    setTimeout(getData, ms);
+}
